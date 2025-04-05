34e0ccf114288affd4b6511f4bf304d41a3826cf
rohit0297 <101257050+rohit0297@users.noreply.github.com>
2025-04-05 18:29:27 +0530

    Update exploratory_data_analysis.sql

--- exploratory_data_analysis.sql
+++ exploratory_data_analysis.sql
@@ -29,3 +29,78 @@ SELECT
 FROM
   `cyclistic-case-study-455810.tripdata.combined_tripdata`;
 
+-- checking the length of ride_id
+SELECT  
+  LENGTH(ride_id) AS length_ride_id,
+  COUNT(*) AS no_of_rows
+FROM
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+GROUP BY length_ride_id;
+
+-- finding out types of bikes and their count
+SELECT 
+  DISTINCT rideable_type,
+  COUNT(rideable_type) AS no_of_trips
+FROM 
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+GROUP BY rideable_type;
+
+-- 
+
+SELECT 
+  started_at, 
+  ended_at
+FROM 
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+LIMIT 10;
+
+
+-- number of bikes which were used for longer duration than a day
+SELECT COUNT(*) AS longer_than_a_day
+FROM
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+WHERE (
+  EXTRACT(HOUR FROM(ended_at - started_at)) * 60 +
+  EXTRACT(MINUTE FROM(ended_at - started_at)) +
+  EXTRACT(SECOND FROM(ended_at - started_at)) / 60) >= 1440;
+
+-- number of bikes which were used for less than a minute
+SELECT COUNT(*) AS less_than_a_min
+FROM
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+WHERE (
+  EXTRACT(HOUR FROM(ended_at - started_at)) * 60 +
+  EXTRACT(MINUTE FROM(ended_at - started_at)) +
+  EXTRACT(SECOND FROM(ended_at - started_at)) / 60) <=1;
+
+
+SELECT DISTINCT start_station_name
+FROM
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+ORDER BY start_station_name;
+
+-- total number of rides which are missing start station name or id
+SELECT COUNT(*) AS rides_with_start_station_missing
+FROM
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+WHERE
+  start_station_name IS NULL 
+  OR start_station_id IS NULL;
+
+
+-- total number of rides which are missing end station name or id
+SELECT COUNT(*) AS rides_with_end_station_missing
+FROM
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+WHERE
+  end_station_name IS NULL 
+  OR end_station_id IS NULL;
+
+-- types of users and no. of trips they took
+SELECT 
+  DISTINCT member_casual,
+  COUNT(*) AS no_of_trips
+FROM
+  `cyclistic-case-study-455810.tripdata.combined_tripdata`
+GROUP BY 
+  member_casual;
